9c126f6af823a71c31f46c47092eb910fa999652
Vitaly Litvinovich <witodivaro@gmail.com>
2021-03-03 19:09:04 +0300

    chore: add splash screen to ios & android

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import DrawerNavigator from './src/navigators/drawer/drawer.navigator.js';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaView style={styles.screen}>
       <DrawerNavigator />
